7ea80d1c86d5b7d0e87e6005a46817d79097ef05
Nathan Witmer <nwitmer@gmail.com>
2010-02-09 11:55:54 -0700

    Lowering dependency on rack to 1.0 for rails 2.3.5 compatibility

--- lib/rack/streaming_proxy.rb
+++ lib/rack/streaming_proxy.rb
@@ -4,7 +4,7 @@ module Rack
     class Error < StandardError; end
 
     # :stopdoc:
-    VERSION = '1.0.2'
+    VERSION = '1.0.3'
     LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
     PATH = ::File.expand_path(::File.join(::File.dirname(__FILE__), "..", "..")) + ::File::SEPARATOR
     # :startdoc:
