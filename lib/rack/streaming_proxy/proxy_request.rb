8ece400f929950c080124d5800922094425ca7a5
Fred Ngo <fredngo@gmail.com>
2013-04-16 15:40:58 +0200

    Add retry if a 503 is experienced.

--- lib/rack/streaming_proxy/proxy_request.rb
+++ lib/rack/streaming_proxy/proxy_request.rb
@@ -2,12 +2,17 @@ require 'logger'
 
 class Rack::StreamingProxy
   class ProxyRequest
+
+    class HTTPServiceUnavailable < RuntimeError; end
+
+    MAX_503_RETRIES = 5
+
     include Rack::Utils
 
     attr_reader :status, :headers
 
     def initialize(request, uri, logger)
-	  	@logger = logger ? logger : Logger.new(STDOUT)
+      @logger = logger ? logger : Logger.new(STDOUT)
       uri = URI.parse(uri)
 
       @logger.info "Proxying to: #{uri.to_s}"
@@ -27,31 +32,49 @@ class Rack::StreamingProxy
       proxy_request["X-Forwarded-For"] =
         (request.env["X-Forwarded-For"].to_s.split(/, +/) + [request.env["REMOTE_ADDR"]]).join(", ")
 
-	  	@logger.debug "[Rack::StreamingProxy] Proxy Request Headers:"
-	  	proxy_request.each_header {|h,v| @logger.debug "[Rack::StreamingProxy] #{h} = #{v}"}
+      @logger.debug "[Rack::StreamingProxy] Proxy Request Headers:"
+      proxy_request.each_header {|h,v| @logger.debug "[Rack::StreamingProxy] #{h} = #{v}"}
       @piper = Servolux::Piper.new 'r', :timeout => 30
 
       @piper.child do
         http_req = Net::HTTP.new(uri.host, uri.port)
         http_req.use_ssl = uri.is_a?(URI::HTTPS)
-        http_req.start do |http|
-	  			@logger.debug "[Rack::StreamingProxy] Starting request to #{http.inspect}"
-	  			
-          http.request(proxy_request) do |response|
-	  				@logger.debug "[Rack::StreamingProxy] got response: #{response.inspect}"
-            # at this point the headers and status are available, but the body
-            # has not yet been read. start reading it and putting it in the parent's pipe.
-            response_headers = {}
-            response.each_header {|k,v| response_headers[k] = v}
-            @piper.puts response.code.to_i
-            @piper.puts response_headers
 
-            response.read_body do |chunk|
-              @piper.puts chunk
+        http_req.start do |http|
+          @logger.debug "[Rack::StreamingProxy] Starting request to #{http.inspect}"
+
+          # Retry the request up to MAX_503_RETRIES times if a 503 is experienced.
+          # This is because Heroku sometimes gives spurious 503 errors that resolve themselves quickly.
+          # do...while loop as suggested by Matz: http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/6745
+          retries = 1
+          success = false
+          loop do
+            http.request(proxy_request) do |response|
+              # at this point the headers and status are available, but the body has not yet been read.
+
+              @logger.debug "[Rack::StreamingProxy] Got response: #{response.inspect}"
+
+              if response.code.to_i == 503
+                if retries <= MAX_503_RETRIES
+                  @logger.debug "[Rack::StreamingProxy] Got 503, retrying (Retry ##{retries})"
+                  sleep 1
+                  retries += 1
+                  next
+                else
+                  raise HTTPServiceUnavailable
+                end
+              end
+
+              # Start reading the body and putting it in the parent's pipe.
+              puts_from_child(response)
+              success = true
             end
-            @piper.puts :done
+
+            break if success
           end
+
         end
+
       end
 
       @piper.parent do
@@ -59,6 +82,7 @@ class Rack::StreamingProxy
         @status = read_from_child
         @headers = HeaderHash.new(read_from_child)
       end
+
     rescue => e
       if @piper
         @piper.parent { raise }
@@ -90,7 +114,20 @@ class Rack::StreamingProxy
     end
 
 
-    protected
+  protected
+
+    def puts_from_child(response)
+      response_headers = {}
+      response.each_header {|k,v| response_headers[k] = v}
+
+      @piper.puts response.code.to_i
+      @piper.puts response_headers
+
+      response.read_body do |chunk|
+        @piper.puts chunk
+      end
+      @piper.puts :done
+    end
 
     def read_from_child
       val = @piper.gets
@@ -104,7 +141,7 @@ class Rack::StreamingProxy
         fixed_name = reconstructed_header_name_for name
         # @logger.debug "Setting proxy header #{name} to #{value} using #{fixed_name}"
         proxy_request[fixed_name] = value unless fixed_name.downcase == "host"
-	  	}
+      }
     end
 
     def reconstructed_header_name_for(rackified_header_name)
